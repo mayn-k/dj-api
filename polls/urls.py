03c0b101ae00fc45dc64735550108c40676229e4
mayn-k <mayank007mk7@gmail.com>
2023-06-30 11:19:24 +0530

    Views with APIView

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import  polls_list, polls_detail
+# from .views import  polls_list, polls_detail
 
+from .apiviews import PollList, PollDetail
+
+
+# URL PATTERNS FOR DJANGO VIEWS
+# urlpatterns = [
+#     path("polls/", polls_list, name="polls_list"),
+#     path("polls/<int:pk>/", polls_detail, name="polls_detail")
+# ]
+
+# URL PATTERNS FOR DRF APIVIEW
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+    path("polls/", PollList.as_view(), name="polls_list"),
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail")
 ]
